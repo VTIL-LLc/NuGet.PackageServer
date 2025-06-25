4944b9ea379351b1877b45fd5e9d78805d29f15b
ErringPaladin10-GITDESKTOP <ErringPaladin10@vtilserver.com>
2025-06-25 01:21:57 -0400

    Some misc changes

--- PackageServer/App_Start/NuGetODataConfig.cs
+++ PackageServer/App_Start/NuGetODataConfig.cs
@@ -1,17 +1,26 @@
+// System includes.
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using System.Web.Http.Routing;
+
+// NuGet includes.
 using NuGet.Server;
 using NuGet.Server.Infrastructure;
 using NuGet.Server.V2;
 
+/// <summary>
+/// Sets this file as the primary startup file for the server.
+/// </summary>
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PackageServer.App_Start.NuGetODataConfig), "Start")]
 
 namespace PackageServer.App_Start 
 {
     public static class NuGetODataConfig 
     {
+        /// <summary>
+        /// Start everything!
+        /// </summary>
         public static void Start() 
         {
             ServiceResolver.SetServiceResolver(new DefaultServiceResolver());
@@ -30,6 +39,7 @@ namespace PackageServer.App_Start
             // Trace.Listeners.Add(new TextWriterTraceListener(HostingEnvironment.MapPath("~/NuGet.Server.log")));
             // Trace.AutoFlush = true;
 
+            // Map those routes!
             config.Routes.MapHttpRoute(
                 name: "NuGetDefault_ClearCache",
                 routeTemplate: "nuget/clear-cache",
--- PackageServer/Properties/AssemblyInfo.cs
+++ PackageServer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PackageServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NuGet Package Server")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VTIL LLc")]
 [assembly: AssemblyProduct("PackageServer")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.2.1.5")]
+[assembly: AssemblyFileVersion("1.2.1.5")]
